69ee30b4e7f1a65ea72f9aaffbe8900a5fd4b58f
jecaro <jeancharles.quillet@gmail.com>
2023-04-06 21:14:30 +0200

    implement basic nc with the server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 use clap::Parser;
+use std::io;
+use std::io::BufRead;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::io::BufReader;
+use tokio::net::TcpStream;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Options {
     #[arg(short, long)]
     server: String,
-    #[arg(short, long, default_value_t = 9000)]
+    #[arg(short, long, default_value_t = 9090)]
     port: u16,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let options = Options::parse();
-    println!("server: {}, port: {}", options.server, options.port);
+
+    let address = format!("{}:{}", options.server, options.port);
+    let stream = TcpStream::connect(address).await?;
+    let mut reader = BufReader::new(stream);
+
+    loop {
+        let mut buf: Vec<u8> = Vec::new();
+        io::stdin().lock().read_until(b'\n', &mut buf)?;
+
+        reader.write_all(&buf).await?;
+
+        let mut reply = String::new();
+        reader.read_line(&mut reply).await?;
+        print!("{reply}");
+    }
 }
