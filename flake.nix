2037222b3a5e7610098fb6630cb72861c2b443c5
jecaro <jeancharles.quillet@gmail.com>
2023-04-21 11:22:54 +0200

    start squeezelite in the background

--- flake.nix
+++ flake.nix
@@ -35,6 +35,7 @@
               rust-analyzer
               rustc
               rustfmt
+              squeezelite-pulse
             ];
         };
       }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use anyhow::Result;
+use anyhow::{bail, Ok, Result};
 use clap::{command, Parser};
 use mpris::connect;
 mod lms_client;
 mod mpris;
+use tokio::process::Command;
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Options {
     #[arg(short = 'H', long)]
@@ -13,17 +14,26 @@ struct Options {
     port: u16,
     #[arg(short, long)]
     player_name: String,
+    #[arg(last = true, default_values_t = vec!["squeezelite-pulse".to_string()])]
+    player_command: Vec<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let options = Options::parse();
 
+    let (player_command, player_args) = match options.player_command[..] {
+        [] => bail!("No player command given"),
+        [ref player_command, ref player_args @ ..] => Ok((player_command, player_args)),
+    }?;
+
+    let mut player_process = Command::new(player_command).args(player_args).spawn()?;
+
     let _connection = connect(options.hostname, options.port, options.player_name.clone()).await?;
 
-    loop {
-        // do something else, wait forever or timeout here:
-        // handling D-Bus messages is done in the background
-        std::future::pending::<()>().await;
+    let exit_status = player_process.wait().await?;
+    match exit_status.code() {
+        Some(code) => bail!("Player exited with code {}", code),
+        None => bail!("Player exited without code"),
     }
 }
