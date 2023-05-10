88bd1f07664d469bc7cf49fa717b5adf46bea0d6
jecaro <jeancharles.quillet@gmail.com>
2023-05-10 17:11:12 +0200

    add the nix overlay

--- flake.nix
+++ flake.nix
@@ -8,36 +8,48 @@
   };
 
   outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
-
-        naersk' = pkgs.callPackage naersk { };
-
+    let derivation = pkgs:
+      let naersk' = pkgs.callPackage naersk { };
       in
-      rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = with pkgs; [ openssl ];
-        };
+      naersk'.buildPackage {
+        src = ./.;
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+        buildInputs = with pkgs; [ openssl ];
+      };
 
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs;
-            [
-              cargo
-              openssl
-              pkg-config
-              rust-analyzer
-              rustc
-              rustfmt
-              squeezelite-pulse
-            ];
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+          };
+
+          naersk' = pkgs.callPackage naersk { };
+
+        in
+        rec {
+          # For `nix build` & `nix run`:
+          defaultPackage = derivation pkgs;
+
+          # For `nix develop`:
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs;
+              [
+                cargo
+                openssl
+                pkg-config
+                rust-analyzer
+                rustc
+                rustfmt
+                squeezelite-pulse
+              ];
+          };
+        }
+      ) // {
+      overlay = final: prev:
+        {
+          mprisqueeze = derivation final;
         };
-      }
-    );
+    };
 }
