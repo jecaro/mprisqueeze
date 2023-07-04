72135f241f2ae08dfc260b28832e03af2cfb22b7
jecaro <jeancharles.quillet@gmail.com>
2023-07-04 22:00:32 +0200

    bump nixpkgs to 23.05

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
     naersk.url = "github:nix-community/naersk";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, flake-utils, naersk, nixpkgs }:
-    let derivation = pkgs:
-      let naersk' = pkgs.callPackage naersk { };
-      in
-      naersk'.buildPackage {
-        src = ./.;
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-        buildInputs = with pkgs; [ openssl ];
-      };
+    let
+      derivation = pkgs:
+        let naersk' = pkgs.callPackage naersk { };
+        in
+        naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [ openssl ];
+        };
 
     in
     flake-utils.lib.eachDefaultSystem
@@ -25,10 +25,8 @@
             inherit system;
           };
 
-          naersk' = pkgs.callPackage naersk { };
-
         in
-        rec {
+        {
           # For `nix build` & `nix run`:
           defaultPackage = derivation pkgs;
 
