1f0d902bdc36f87d7422aaf5ced56746d4b916e5
jecaro <jeancharles.quillet@gmail.com>
2023-07-19 11:52:52 +0200

    build with rustls-tls

--- flake.nix
+++ flake.nix
@@ -13,8 +13,6 @@
         in
         naersk'.buildPackage {
           src = ./.;
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = with pkgs; [ openssl ];
         };
 
     in
@@ -35,8 +33,6 @@
             nativeBuildInputs = with pkgs;
               [
                 cargo
-                openssl
-                pkg-config
                 rust-analyzer
                 rustc
                 rustfmt
