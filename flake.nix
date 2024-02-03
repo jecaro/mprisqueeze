df6a9526ee9d4c7d09f5a67a0bb93dc65b06a62c
jecaro <jeancharles.quillet@gmail.com>
2024-02-03 15:37:16 +0100

    Add cargo-edit to dev dependencies

--- flake.nix
+++ flake.nix
@@ -33,6 +33,7 @@
             nativeBuildInputs = with pkgs;
               [
                 cargo
+                cargo-edit
                 rust-analyzer
                 rustc
                 rustfmt
