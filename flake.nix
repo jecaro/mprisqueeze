4ffdfb2409346eb13ae544a7bed577ae4257a7e5
jecaro <jeancharles.quillet@gmail.com>
2025-12-06 21:18:59 +0100

    Bump to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
   };
