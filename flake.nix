ce505e5c43671e12971addc01b81da8748ea805a
jecaro <jeancharles.quillet@gmail.com>
2026-01-10 15:09:03 +0100

    Bump to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
   };
